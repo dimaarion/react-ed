6653f5829f7b05d1a225668fdbdc96e46d82bdc9
dimaarion <dimaarion@gmail.com>
2020-09-04 00:09:57 +0300

    1 file modified

--- src/ImageRemove.js
+++ src/ImageRemove.js
@@ -5,26 +5,43 @@ function ImageRemove(props) {
     <div
       className="image_remove"
       style={{
-        marginLeft: props.mouseImg.x + 20 + 'px',
-        marginTop: props.mouseImg.y - 100 + 'px'
+        marginLeft: props.mouseImg.x + 20 + "px",
+        marginTop: props.mouseImg.y - 100 + "px"
       }}
     >
       <div>
-       <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-
+      <h3>Редактирование</h3> 
+      <h3>изображения</h3>
+        <div className="form-group">
+          <label for="width">Email address</label>
+          <input
+            type="text"
+            class="form-control"
+            id="width"
+           
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="form-group form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label class="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
       </div>
     </div>
   );
