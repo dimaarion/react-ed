7e41f580bf16278f1cd9d1294e4086b9378d9a90
dimaarion <dimaarion@gmail.com>
2020-09-04 07:55:12 +0300

    1 file modified

--- src/ImageRemove.js
+++ src/ImageRemove.js
@@ -13,33 +13,33 @@ function ImageRemove(props) {
       <h3>Редактирование</h3> 
       <h3>изображения</h3>
         <div className="form-group">
-          <label for="width">Email address</label>
+          <label htmlFor="width">Email address</label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="width"
            
           />
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
           <input
             type="password"
             className="form-control"
             id="exampleInputPassword1"
           />
         </div>
-        <div class="form-group form-check">
+        <div className="form-group form-check">
           <input
             type="checkbox"
             className="form-check-input"
             id="exampleCheck1"
           />
-          <label class="form-check-label" for="exampleCheck1">
+          <label className="form-check-label" htmlFor="exampleCheck1">
             Check me out
           </label>
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </div>
